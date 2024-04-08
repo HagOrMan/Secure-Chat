1a3efe020ce3a8bc53cdf58898defac3532d223a
jayshharma <jaybaps12@gmail.com>
2024-04-08 09:49:37 -0400

    send invite navigation

--- Frontend/app/src/main/java/com/example/frontend/ScheduleMeeting.java
+++ Frontend/app/src/main/java/com/example/frontend/ScheduleMeeting.java
@@ -3,6 +3,7 @@ package com.example.frontend;
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -99,6 +100,8 @@ public class ScheduleMeeting extends AppCompatActivity {
         sendInviteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent sendInviteNav = new Intent(ScheduleMeeting.this, ChatList.class);
+                startActivity(sendInviteNav);
                 sendInvite();
             }
         });
